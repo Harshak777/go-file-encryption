5db56654ef5868b65e70943da6297017657c4248
Harshak777 <harshakkrishnaa777@gmail.com>
2024-04-25 15:18:40 -0700

    Used term package to get user's password

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module harshak777/go-file-encrption
 
 go 1.20
+
+require golang.org/x/term v0.19.0
+
+require golang.org/x/sys v0.19.0 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+
+	"golang.org/x/term"
 )
 
 func main() {
@@ -38,6 +41,20 @@ func printHelp() {
 }
 
 func encyptHandle() {
+	if len(os.Args) < 3 {
+		fmt.Println("Missing the path to the file. For more info use the help command.")
+		os.Exit(0)
+	}
+
+	file := os.Args[2]
+
+	if !validateFile(file) {
+		panic("File not found")
+	}
+
+	_ = getPassword()
+
+	fmt.Println("___ENCRYPTING___")
 
 }
 
@@ -45,14 +62,31 @@ func decryptHandle() {
 
 }
 
-func getPassword() {
+func getPassword() []byte {
+	fmt.Println("Enter the password.")
+	password, _ := term.ReadPassword(0)
 
+	fmt.Println("Confirm the password.")
+	confirmPassword, _ := term.ReadPassword(0)
+
+	if validatePassword(password, confirmPassword) {
+		fmt.Println("Passwords didn't match")
+		getPassword()
+	}
+
+	return password
 }
 
-func validatePassword() {
-
+func validatePassword(password []byte, confirmPassword []byte) bool {
+	if !bytes.Equal(password, confirmPassword) {
+		return false
+	}
+	return true
 }
 
-func validateFile() {
-
+func validateFile(file string) bool {
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
